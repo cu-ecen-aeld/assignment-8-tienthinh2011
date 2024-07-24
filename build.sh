784bfa0866f0af8955311cf2e89bcbd0e344720d
Thinh Nguyen <tienthinh2011@gmail.com>
2024-07-24 09:57:41 +0000

    Tested build ldd modules

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@
 source shared.sh
 
 EXTERNAL_REL_BUILDROOT=../base_external
+EXTERNAL_REL__ROOTFS_OVERLAY=../base_external/rootfs_overlay
 git submodule init
 git submodule sync
 git submodule update
@@ -23,11 +24,11 @@ then
 	if [ -e ${AESD_MODIFIED_DEFCONFIG} ]
 	then
 		echo "USING ${AESD_MODIFIED_DEFCONFIG}"
-		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT}
+		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT} BR2_ROOTFS_OVERLAY=${EXTERNAL_REL__ROOTFS_OVERLAY}
 	else
 		echo "Run ./save_config.sh to save this as the default configuration in ${AESD_MODIFIED_DEFCONFIG}"
 		echo "Then add packages as needed to complete the installation, re-running ./save_config.sh as needed"
-		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_DEFAULT_DEFCONFIG}
+		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_DEFAULT_DEFCONFIG} BR2_ROOTFS_OVERLAY=${EXTERNAL_REL__ROOTFS_OVERLAY}
 	fi
 else
 	echo "USING EXISTING BUILDROOT CONFIG"
