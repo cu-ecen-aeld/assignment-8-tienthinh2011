0f98e90a781c9d050e5a4ae41c17b48d81139812
Thinh Nguyen <tienthinh2011@gmail.com>
2024-08-09 04:10:29 +0000

    assignment-8: fix commit hash

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -7,7 +7,8 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 # AESD_ASSIGNMENTS_VERSION = 'assignment-5-buildroot'
-AESD_ASSIGNMENTS_VERSION = 'assignment-8' 
+AESD_ASSIGNMENTS_VERSION = '9f9893e'#'assignment-8' 
+
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
